e71c952b042aef0e610b661137970ff1ca45edb9
YMODI061 <ymodi061@gmail.com>
2026-04-07 16:48:13 +0530

    Create tailwind.config.js with custom ZIV color palette

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        dustyRose: '#D09FB6',
+        cream: '#FFFDD0',
+        mocha: '#4B3C35',
+        satinGold: '#F6E89E',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
